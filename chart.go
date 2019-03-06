20a1c5d7604ad5f0a5d81956311b0ba77e5a40bf
Cory G Watson <cwatson@signalfx.com>
2019-03-06 08:38:54 -0800

    Use OpenAPI codegen models

--- chart.go
+++ chart.go
@@ -7,119 +7,16 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+
+	"github.com/signalfx/signalfx-go/chart"
 )
 
 // ChartAPIURL is the base URL for interacting with charts.
 const ChartAPIURL = "/v2/chart"
 
-// Chart is a chart.
-type Chart struct {
-	Created          int64  `json:"created,omitempty"`
-	Creator          string `json:"creator,omitempty"`
-	CustomProperties string `json:"customProperties,omitempty"`
-	Description      string `json:"description,omitempty"`
-	LastUpdated      int64  `json:"lastUpdated,omitEmpty"`
-	LastUpdatedBy    string `json:"lastUpdatedBy,omitEmpty"`
-	Name             string `json:"name"`
-	Options          struct {
-		AreaChartOptions struct {
-			ShowDataMarkers bool `json:"showDataMarkers,omitempty"`
-		} `json:"areaChartOptions,omitempty"`
-		Axes []struct {
-			HighWaterMark      int64  `json:"highWaterMark,omitempty"`
-			HighWaterMarkLabel string `json:"highWaterMarkLabel,omitempty"`
-			Label              string `json:"label,omitempty"`
-			LowWaterMarkLabel  string `json:"lowWaterMarkLabel,omitempty"`
-			LowhWaterMark      int64  `json:"lowhWaterMark,omitempty"`
-			Max                int64  `json:"max,omitempty"`
-			Min                int64  `json:"min,omitempty"`
-		} `json:"axes,omitempty"`
-		AxisPrecision int64  `json:"axisPrecision,omitempty"`
-		ColorBy       string `json:"colorBy,omitempty"`
-		ColorRange    struct {
-			Color string `json:"color,omitempty"`
-			Max   string `json:"max,omitempty"`
-			Min   string `json:"min,omitempty"`
-		} `json:"colorRange,omitempty"`
-		ColorScale struct {
-			Inverted   bool    `json:"inverted,omitempty"`
-			Thresholds []int64 `json:"thresholds,omitempty"`
-		} `json:"colorScale,omitempty"`
-		ColorScale2 []struct {
-			Gt           int64 `json:"gt,omitempty"`
-			Gte          int64 `json:"gte,omitempty"`
-			Lt           int64 `json:"lt,omitempty"`
-			Lte          int64 `json:"lte,omitempty"`
-			PaletteIndex int64 `json:"paletteIndex,omitempty"`
-		} `json:"colorScale2,omitempty"`
-		DefaultPlotType       string   `json:"defaultPlotType,omitempty"`
-		GroupBy               []string `json:"groupBy,omitempty"`
-		HistogramChartOptions struct {
-			ColorThemeIndex int64 `json:"colorThemeIndex,omitempty"`
-		} `json:"histogramChartOptions,omitempty"`
-		IncludeZero   bool `json:"includeZero,omitempty"`
-		LegendOptions struct {
-			Fields []struct {
-				Enabled  bool   `json:"enabled,omitempty"`
-				Property string `json:"property,omitempty"`
-			} `json:"fields,omitempty"`
-		} `json:"legendOptions,omitempty"`
-		LineChartOptionsObject struct {
-			ShowDataMarkers bool `json:"showDataMarkers,omitempty"`
-		} `json:"lineChartOptionsObject,omitempty"`
-		Markdown             string `json:"markdown,omitempty"`
-		MaximumPrecision     int64  `json:"maximumPrecision,omitempty"`
-		OnChartLegendOptions struct {
-			DimensionInLegend string `json:"dimensionInLegend,omitempty"`
-			ShowLegend        bool   `json:"showLegend,omitempty"`
-		} `json:"onChartLegendOptions,omitempty"`
-		ProgramOptions struct {
-			DisableSampling   bool  `json:"disableSampling,omitempty"`
-			MaxDelay          int64 `json:"maxDelay,omitempty"`
-			MinimumResolution int64 `json:"minimumResolution,omitempty"`
-		} `json:"programOptions,omitempty"`
-		PublishLabelOptions []struct {
-			DisplayName  string `json:"displayName,omitempty"`
-			Label        string `json:"label,omitempty"`
-			PaletteIndex int64  `json:"paletteIndex,omitempty"`
-			PlotType     string `json:"plotType,omitempty"`
-			ValuePrefix  string `json:"valuePrefix,omitempty"`
-			ValueSuffix  string `json:"valueSuffix,omitempty"`
-			ValueUnit    string `json:"valueUnit,omitempty"`
-			YAxis        int64  `json:"yAxis,omitempty"`
-		} `json:"publishLabelOptions,omitempty"`
-		RefreshInterval        int64  `json:"refreshInterval,omitempty"`
-		SecondaryVisualization string `json:"secondaryVisualization,omitempty"`
-		ShowEventLines         bool   `json:"showEventLines,omitempty"`
-		ShowSparkLine          bool   `json:"showSparkLine,omitempty"`
-		SortBy                 string `json:"sortBy,omitempty"`
-		SortDirection          string `json:"sortDirection,omitempty"`
-		SortProperty           string `json:"sortProperty,omitempty"`
-		Stacked                bool   `json:"stacked,omitempty"`
-		Time                   struct {
-			End   int64  `json:"end,omitempty"`
-			Range int64  `json:"range,omitempty"`
-			Start int64  `json:"start,omitempty"`
-			Type  string `json:"type,omitempty"`
-		} `json:"time,omitempty"`
-		TimeStampHidden bool   `json:"timeStampHidden,omitempty"`
-		Type            string `json:"type,omitempty"`
-		UnitPrefix      string `json:"unitPrefix,omitempty"`
-	} `json:"options,omitempty"`
-	PackageSpecifications string   `json:"packageSpecifications,omitempty"`
-	ProgramText           string   `json:"programText,omitempty"`
-	Tags                  []string `json:"tags,omitempty"`
-}
-
-// ChartSearch is the result of a query for Charts
-type ChartSearch struct {
-	Count   int64 `json:"count,omitempty"`
-	Results []Chart
-}
-
 // CreateChart creates a chart.
-func (c *Client) CreateChart(chart *Chart) (*Chart, error) {
-	payload, err := json.Marshal(chart)
+func (c *Client) CreateChart(chartRequest *chart.CreateUpdateChartRequest) (*chart.CreateUpdateChartResponse, error) {
+	payload, err := json.Marshal(chartRequest)
 	if err != nil {
 		return nil, err
 	}
@@ -130,7 +27,7 @@ func (c *Client) CreateChart(chart *Chart) (*Chart, error) {
 	}
 	defer resp.Body.Close()
 
-	finalChart := &Chart{}
+	finalChart := &chart.CreateUpdateChartResponse{}
 
 	err = json.NewDecoder(resp.Body).Decode(finalChart)
 
@@ -154,7 +51,7 @@ func (c *Client) DeleteChart(id string) error {
 }
 
 // GetChart gets a chart.
-func (c *Client) GetChart(id string) (*Chart, error) {
+func (c *Client) GetChart(id string) (*chart.GetSingleChartResponse, error) {
 	resp, err := c.doRequest("GET", ChartAPIURL+"/"+id, nil, nil)
 
 	if err != nil {
@@ -162,7 +59,7 @@ func (c *Client) GetChart(id string) (*Chart, error) {
 	}
 	defer resp.Body.Close()
 
-	finalChart := &Chart{}
+	finalChart := &chart.GetSingleChartResponse{}
 
 	err = json.NewDecoder(resp.Body).Decode(finalChart)
 
@@ -170,8 +67,8 @@ func (c *Client) GetChart(id string) (*Chart, error) {
 }
 
 // UpdateChart updates a chart.
-func (c *Client) UpdateChart(id string, chart *Chart) (*Chart, error) {
-	payload, err := json.Marshal(chart)
+func (c *Client) UpdateChart(id string, chartRequest *chart.CreateUpdateChartRequest) (*chart.CreateUpdateChartResponse, error) {
+	payload, err := json.Marshal(chartRequest)
 	if err != nil {
 		return nil, err
 	}
@@ -182,7 +79,7 @@ func (c *Client) UpdateChart(id string, chart *Chart) (*Chart, error) {
 	}
 	defer resp.Body.Close()
 
-	finalChart := &Chart{}
+	finalChart := &chart.CreateUpdateChartResponse{}
 
 	err = json.NewDecoder(resp.Body).Decode(finalChart)
 
@@ -190,7 +87,7 @@ func (c *Client) UpdateChart(id string, chart *Chart) (*Chart, error) {
 }
 
 // SearchChart searches for charts, given a query string in `name`.
-func (c *Client) SearchChart(limit int, name string, offset int, tags string) (*ChartSearch, error) {
+func (c *Client) SearchCharts(limit int, name string, offset int, tags string) (*chart.GetChartsResult, error) {
 	params := url.Values{}
 	params.Add("limit", strconv.Itoa(limit))
 	params.Add("name", name)
@@ -204,7 +101,7 @@ func (c *Client) SearchChart(limit int, name string, offset int, tags string) (*
 	}
 	defer resp.Body.Close()
 
-	finalCharts := &ChartSearch{}
+	finalCharts := &chart.GetChartsResult{}
 
 	err = json.NewDecoder(resp.Body).Decode(finalCharts)
 
--- client_test.go
+++ client_test.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
-	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -64,72 +63,4 @@ func verifyRequest(t *testing.T, method string, status int, params url.Values, r
 			fmt.Fprintf(w, fixture(resultPath))
 		}
 	}
-}
-
-func TestCreateChart(t *testing.T) {
-	teardown := setup()
-	defer teardown()
-
-	mux.HandleFunc("/v2/chart", verifyRequest(t, "POST", http.StatusOK, nil, "chart/create_success.json"))
-
-	result, err := client.CreateChart(&Chart{
-		Name: "string",
-	})
-	assert.NoError(t, err, "Unexpected error creating chart")
-	assert.Equal(t, "string", result.Name, "Name does not match")
-}
-
-func TestDeleteChart(t *testing.T) {
-	teardown := setup()
-	defer teardown()
-
-	mux.HandleFunc("/v2/chart/string", verifyRequest(t, "DELETE", http.StatusOK, nil, ""))
-
-	err := client.DeleteChart("string")
-	assert.NoError(t, err, "Unexpected error getting chart")
-}
-
-func TestGetChart(t *testing.T) {
-	teardown := setup()
-	defer teardown()
-
-	mux.HandleFunc("/v2/chart/string", verifyRequest(t, "GET", http.StatusOK, nil, "chart/get_success.json"))
-
-	result, err := client.GetChart("string")
-	assert.NoError(t, err, "Unexpected error getting chart")
-	assert.Equal(t, result.Name, "string", "Name does not match")
-}
-
-func TestSearchChart(t *testing.T) {
-	teardown := setup()
-	defer teardown()
-
-	limit := 10
-	name := "foo"
-	offset := 2
-	tags := "bar"
-	params := url.Values{}
-	params.Add("limit", strconv.Itoa(limit))
-	params.Add("name", name)
-	params.Add("offset", strconv.Itoa(offset))
-	params.Add("tags", tags)
-
-	mux.HandleFunc("/v2/chart", verifyRequest(t, "GET", http.StatusOK, params, "chart/get_success.json"))
-
-	results, err := client.SearchChart(limit, name, offset, tags)
-	assert.NoError(t, err, "Unexpected error search chart")
-	assert.Equal(t, int64(0), results.Count, "Incorrect number of results")
-}
-
-func TestUpdateChart(t *testing.T) {
-	teardown := setup()
-	defer teardown()
-
-	mux.HandleFunc("/v2/chart/string", verifyRequest(t, "PUT", http.StatusOK, nil, "chart/update_success.json"))
-
-	result, err := client.UpdateChart("string", &Chart{
-		Name: "string",
-	})
-	assert.NoError(t, err, "Unexpected error updating chart")
-	assert.Equal(t, "string", result.Name, "Name does not match")
 }
