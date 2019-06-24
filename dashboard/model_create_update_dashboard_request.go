799ea2649336f4dd109d2799ff05287aa8f6a8bb
Cory G Watson <cwatson@signalfx.com>
2019-06-24 13:27:50 -0700

    Fix busted create dashboard chart entry

--- dashboard/model_create_update_dashboard_request.go
+++ dashboard/model_create_update_dashboard_request.go
@@ -9,14 +9,10 @@
 
 package dashboard
 
-import (
-	"github.com/signalfx/signalfx-go/chart"
-)
-
 type CreateUpdateDashboardRequest struct {
 	AuthorizedWriters *AuthorizedWriters     `json:"authorizedWriters,omitempty"`
 	ChartDensity      *DashboardChartDensity `json:"chartDensity,omitempty"`
-	Charts            []*chart.Chart         `json:"charts,omitempty"`
+	Charts            []*DashboardChart      `json:"charts,omitempty"`
 	// Description of the dashboard. The system displays the value in the dashboard tab tooltip in the dashboard group in the web UI.
 	Description string `json:"description,omitempty"`
 	// Array of event overlay definitions that you can apply to all of the charts of this dashboard. When you apply the overlays, the system displays all the active events that match the specified search term and any specified filter on all the charts in the dashboard. The display uses the color you specify for the overlay and, if selected, vertical lines that mark the event.<br> **Note:** The objects in this array correspond to the *suggested* event overlays specified in the web UI, and they're not automatically applied as active overlays. To set default active event overlays, use the `selectedEventOverlays` property instead.
