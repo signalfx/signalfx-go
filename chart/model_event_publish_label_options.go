1ed45e4c9c8aff3824d433b57a84fc5568aceb03
Cory G Watson <cwatson@signalfx.com>
2019-06-27 12:58:45 -0700

    Add event feed options

---
+++ chart/model_event_publish_label_options.go
@@ -0,0 +1,7 @@
+package charts
+
+type EventPublishLabelOptions struct {
+	DisplayName  string `json:"displayName,omitempty"`
+	Label        string `json:"label,omitempty"`
+	PaletteIndex int    `json:"paletteIndex,omitempty"`
+}
