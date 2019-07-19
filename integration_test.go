b72c07a3bd5b504c96161c3f29e912474601c9d9
Cory G Watson <cwatson@signalfx.com>
2019-07-19 12:37:04 -0700

    Add integrations and use for AWS

--- integration_test.go
+++ integration_test.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/signalfx/signalfx-go/integration"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -48,4 +49,51 @@ func TestGetMissingIntegration(t *testing.T) {
 	result, err := client.GetIntegration("string")
 	assert.Error(t, err, "Should get an error getting missing integration")
 	assert.Nil(t, result, "Should get a nil result from a missing integration")
+}
+
+func TestCreateAWSCloudWatchIntegration(t *testing.T) {
+	teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/v2/integration", verifyRequest(t, "POST", http.StatusOK, nil, "integration/create_aws_success.json"))
+
+	result, err := client.CreateAWSCloudWatchIntegration(&integration.AwsCloudWatchIntegration{
+		Type: "AWSCloudWatch",
+	})
+	assert.NoError(t, err, "Unexpected error creating integration")
+	assert.Equal(t, "string", result.Name, "Name does not match")
+}
+
+func TestGetAWSCloudWatchIntegration(t *testing.T) {
+	teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/v2/integration/id", verifyRequest(t, "GET", http.StatusOK, nil, "integration/create_aws_success.json"))
+
+	result, err := client.GetAWSCloudWatchIntegration("id")
+	assert.NoError(t, err, "Unexpected error getting integration")
+	assert.Equal(t, "string", result.Name, "Name does not match")
+}
+
+func TestUpdateAWSCloudWatchIntegration(t *testing.T) {
+	teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/v2/integration/id", verifyRequest(t, "PUT", http.StatusOK, nil, "integration/create_aws_success.json"))
+
+	result, err := client.UpdateAWSCloudWatchIntegration("id", &integration.AwsCloudWatchIntegration{
+		Type: "AWSCloudWatch",
+	})
+	assert.NoError(t, err, "Unexpected error creating integration")
+	assert.Equal(t, "string", result.Name, "Name does not match")
+}
+
+func TestDeleteAWSCloudWatchIntegration(t *testing.T) {
+	teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/v2/integration/id", verifyRequest(t, "DELETE", http.StatusOK, nil, ""))
+
+	err := client.DeleteAWSCloudWatchIntegration("id")
+	assert.NoError(t, err, "Unexpected error creating integration")
 }
