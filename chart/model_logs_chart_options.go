77f814c4c3d970cc162dbdc099c67f3443c9e61f
Siri Singamneni <ssingamneni@splunk.com>
2022-07-22 11:23:23 -0700

    Make logs chart options nested

--- chart/model_logs_chart_options.go
+++ chart/model_logs_chart_options.go
@@ -1,5 +1,14 @@
 package chart
 
+type LogsListChartOptions struct {
+	// The column configuration on logs chart
+	Columns *Columns `json:"columns,omitempty"`
+	// The sort option configuration for a logs chart
+	SortOptions *SortOptions `json:"sortOptions,omitempty"`
+	// The default connection configuration for a logs chart 
+	DefaultConnection string `json:"defaultConnection,omitempty"`
+}
+
 type Columns struct {
 	Name string `json:"name,omitempty"`
 }
