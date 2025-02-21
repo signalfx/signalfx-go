f938fcfdf15ec8f9554fdc5aa6809ff2b35767f4
durgeshsplunk <durgesht@splunk.com>
2025-02-21 11:47:29 +0530

    Update notification/model_splunk_platform_notification.go
    Co-authored-by: Antoine Toulme <antoine@toulme.name>

--- notification/model_splunk_platform_notification.go
+++ notification/model_splunk_platform_notification.go
@@ -3,7 +3,7 @@ package notification
 type SplunkPlatformNotification struct {
 	// Type sets which system to use to send the notification. For a Splunk Platform notification, this is always \"SplunkPlatform\".
 	Type string `json:"type"`
-	// Tells SignalFX the HTTP Event Collector (HEC) URI for your Splunk platform instance
+	// Url sets the HTTP Event Collector (HEC) URI for your Splunk platform instance
 	Url string `json:"url"`
 	//Enter the HTTP Event Collector token that allows access to your Splunk platform instance
 	HecToken string `json:"hecToken"`
