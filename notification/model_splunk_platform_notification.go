d385c2b672c4c69ed2ffe6abe43594f43e52d3dc
durgeshsplunk <durgesht@splunk.com>
2025-02-21 12:52:51 +0530

    Update model_splunk_platform_notification.go

--- notification/model_splunk_platform_notification.go
+++ notification/model_splunk_platform_notification.go
@@ -4,7 +4,7 @@ type SplunkPlatformNotification struct {
 	// Type sets which system to use to send the notification. For a Splunk Platform notification, this is always \"SplunkPlatform\".
 	Type string `json:"type"`
 	// Url sets the HTTP Event Collector (HEC) URI for your Splunk platform instance
-	Url string `json:"url,omitempty"`
+	Url string `json:"url"`
 	//HecToken sets the HTTP Event Collector token that allows access to your Splunk platform instance
-	HecToken string `json:"hecToken,omitempty"`
+	HecToken string `json:"hecToken"`
 }
