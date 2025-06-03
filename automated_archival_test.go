894b1b638e61147e5b50bb7217920518230ee776
Anuj Doiphode <adoiphode@splunk.com>
2025-06-03 09:54:22 -0700

    added API implementation for automated archival APIs

---
+++ automated_archival_test.go
@@ -0,0 +1,29 @@
+package signalfx
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	automated_archival "github.com/signalfx/signalfx-go/automated-archival"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateSettings(t *testing.T) {
+	teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc(AutomatedArchivalApiURL+"/settings", verifyRequest(t, http.MethodPost, true, http.StatusOK, nil, "automated_archival/create_settings_success.json"))
+	result, err := client.CreateSettings(context.Background(), &automated_archival.AutomatedArchivalSettings{
+		Version:        1,
+		Enabled:        true,
+		LookbackPeriod: "P60D",
+		GracePeriod:    "P30D",
+	})
+
+	assert.NoError(t, err, "Unexpected error creating Automated Archival settings")
+	assert.Equal(t, 1, result.Version, "Version doesn't match")
+	assert.Equal(t, true, result.Enabled, "Enabled doesn't match")
+	assert.Equal(t, "P60D", result.LookbackPeriod, "Lookback period doesn't match")
+	assert.Equal(t, "P30D", result.GracePeriod, "Grace period doesn't match")
+}
