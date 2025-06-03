3d7cc7006cbb942d8d0fa380d3b41ad10836c045
Anuj Doiphode <adoiphode@splunk.com>
2025-06-03 15:02:13 -0700

    fixing TestGetSettings

--- automated_archival_test.go
+++ automated_archival_test.go
@@ -34,15 +34,14 @@ func TestGetSettings(t *testing.T) {
 
 	mux.HandleFunc(AutomatedArchivalApiURL+"/settings", verifyRequest(t, http.MethodGet, true, http.StatusOK, nil, "automated_archival/get_settings_success.json"))
 
-	var CreatedUpdatedTs = 1674598662022
 	result, err := client.GetSettings(context.Background())
 	assert.NoError(t, err, "Unexpected error getting Automated Archival settings")
 	assert.Equal(t, int64(1), result.Version, "Version doesn't match")
 	assert.Equal(t, true, result.Enabled, "Enabled doesn't match")
 	assert.Equal(t, "P60D", result.LookbackPeriod, "Lookback period doesn't match")
 	assert.Equal(t, "P30D", result.GracePeriod, "Grace period doesn't match")
-	assert.Equal(t, CreatedUpdatedTs, result.Created, "'Created' timestamp doesn't match")
-	assert.Equal(t, CreatedUpdatedTs, result.LastUpdated, "'Last Updated' timestamp doesn't match")
-	assert.Equal(t, "TestCreatorId", result.Creator, "Creator doesn't match")
-	assert.Equal(t, "TestUpdatedId", result.LastUpdatedBy, "'Last Updated By' user doesn't match")
+	assert.Equal(t, int64(1674598662022), *result.Created, "'Created' timestamp doesn't match")
+	assert.Equal(t, int64(1674598662022), *result.LastUpdated, "'Last Updated' timestamp doesn't match")
+	assert.Equal(t, "TestCreatorId", *result.Creator, "Creator doesn't match")
+	assert.Equal(t, "TestUpdatedId", *result.LastUpdatedBy, "'Last Updated By' user doesn't match")
 }
